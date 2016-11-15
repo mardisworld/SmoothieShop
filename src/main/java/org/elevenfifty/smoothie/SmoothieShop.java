ca165495e482a0bcc19d244374b6f398855b13aa
Marissa Staller <mardisworld@gmail.com>
2016-11-15 14:29:24 -0500

    step5

--- src/main/java/org/elevenfifty/smoothie/SmoothieShop.java
+++ src/main/java/org/elevenfifty/smoothie/SmoothieShop.java
@@ -2,12 +2,16 @@ package org.elevenfifty.smoothie;
 
 import java.io.IOException;
 //import java.util.List;
+import java.util.InputMismatchException;
+import java.util.Scanner;
 
 //import org.apache.log4j.Logger;
 import org.elevenfifty.smoothie.beans.Recipe;
 import org.elevenfifty.smoothie.decoratored.beans.Smoothie;
 import org.elevenfifty.smoothie.util.Browser;
 import org.elevenfifty.smoothie.util.PrettyPrinter;
+import static org.elevenfifty.smoothie.util.Inventory.hasSufficientInventory;
+
 
 
 
@@ -15,28 +19,53 @@ public class SmoothieShop {
 	//private static final Logger logger = Logger.getLogger(SmoothieShop.class);
 
 	public static void main(String[] args) throws IOException {
+		final Scanner in = new Scanner(System.in);
 		Configuration config = Configuration.configure("recipes.csv", "ingredients.csv");
-
-		// Recipe Example
-		Recipe r = config.getRecipe("Best Smoothie");
-		PrettyPrinter.print(r);
-
-		//logger.info(printPretty("Ingredients:", r.getIngredients()));
+		
+//private static
+//		// Recipe Example
+//		Recipe r = config.getRecipe("Best Smoothie");
+//		PrettyPrinter.print(r);
+//
+//		//logger.info(printPretty("Ingredients:", r.getIngredients()));
 		//logger.info(printPretty("Instructions:", r.getInstructions()));
 		//logger.info(r.getCost());
 
 		// Decorator Pattern Example
-		Smoothie s = new Smoothie(config.getIngredient("Orange"));
-		s = new Smoothie(config.getIngredient("Banana"), s);
-		PrettyPrinter.print(s);
-
+//		Smoothie s = new Smoothie(config.getIngredient("Orange"));
+//		s = new Smoothie(config.getIngredient("Banana"), s);
+//		PrettyPrinter.print(s);
 
 		Browser browser = new Browser(config);
-		browser.displayRecipes();
-		Recipe selectedRecipe = browser.readRecipe();//*error 
-		selectedRecipe.consumeIngredients();
-		PrettyPrinter.print(selectedRecipe);
-		
+
+		try {
+			while (true) {
+				browser.displayRecipes();
+				Recipe selectedRecipe = browser.readRecipe();
+
+				if (hasSufficientInventory(selectedRecipe)) {
+					selectedRecipe.consumeIngredients();
+					PrettyPrinter.print(selectedRecipe);
+				} else {
+					System.out.println("Insufficient Inventory");
+				}
+			}
+		} catch (InputMismatchException e) {
+			System.out.println("Have a nice day!");
+//			in.nextLine();
+//			if (in.nextLine().equalsIgnoreCase("q")){
+//				System.out.println("Have a nice day!");
+//			} else {
+//				System.out.println("Please enter a valid response from 1-3. ");
+//			}
+		}
+
+//		
+//		browser.displayRecipes();
+//		Recipe selectedRecipe = browser.readRecipe();//*error 
+//		selectedRecipe.consumeIngredients();
+//		PrettyPrinter.print(selectedRecipe);
+//		
 	//	logger.info(printPretty("Ingredients:", s.getIngredients()));
 	//	logger.info(printPretty("Instructions:", s.getInstructions()));
 	//	logger.info(s.getCost());
--- src/main/java/org/elevenfifty/smoothie/util/Browser.java
+++ src/main/java/org/elevenfifty/smoothie/util/Browser.java
@@ -1,9 +1,13 @@
 package org.elevenfifty.smoothie.util;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 import org.elevenfifty.smoothie.Configuration;
 import org.elevenfifty.smoothie.beans.Recipe;
+import static org.elevenfifty.smoothie.util.Inventory.hasSufficientInventory;
+
+
 
 public class Browser {
 	private final Configuration config;
@@ -14,17 +18,47 @@ public class Browser {
 	}
 
 	public void displayRecipes() {
-		//loadRecipes(recipes.csv);
+		System.out.println();
+		System.out.println("Available recipes: ");
 		for (int i = 0; i < config.listRecipes().size(); i++) {
 			Recipe r = config.getRecipe(i);
-			System.out.format("%d: %s $%,01.2f%n", i + 1, r.getName(), r.getCost());
+			
+			if (hasSufficientInventory(r)) {
+				System.out.format("%d: %s $%,01.2f%n", i + 1, r.getName(), r.getCost());
+			} else {
+				System.out.format("%d: %s (insufficient inventory)%n", i + 1, r.getName());
+			}
 		}
+	
+
+		
+		//loadRecipes(recipes.csv);
+//		for (int i = 0; i < config.listRecipes().size(); i++) {
+//			System.out.println();
+//			System.out.println("Available recipes: ");
+//			Recipe r = config.getRecipe(i);
+//			System.out.format("%d: %s $%,01.2f%n", i + 1, r.getName(), r.getCost());
+//		}
 	}
 
 	public Recipe readRecipe() {
 		System.out.println();
-		System.out.print("Select a Smoothie Recipe: ");//It's printing this twice and I don't know why. 
+		System.out.print("Select a Smoothie Recipe(q to quit): ");
+		
+		
 
+//		From step 4
+//		try 
+//		{
+//			index=userIn.nextInt();
+//		}
+//		catch (InputMismatchException e)
+//		{
+//			userIn.nextLine();
+//        	System.out.println("That's not an integer. Try again. \n");
+//        	option=0;
+//		}
+		
 		return config.getRecipe(in.nextInt() - 1);
 	}
 
